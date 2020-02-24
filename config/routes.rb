cd6adb52804e7d12de243f12af2dcaebcb9acee7
oriaki1231 <aki.raliver.1231@icloud.com>
2020-02-24 16:53:47 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
   root to: 'home#index'
+
   resources :souls, only: [:new, :create]
+  resources :guams, only: [:new, :create]
+  resources :taipeis, only: [:new, :create]
+  resources :hawais, only: [:new, :create]
+  resources :hongkongs, only: [:new, :create]
+  resources :balis, only: [:new, :create]
+  resources :bankokus, only: [:new, :create]
+  resources :sebus, only: [:new, :create]
+  resources :singapauls, only: [:new, :create]
+  resources :pusans, only: [:new, :create]
   
 end
