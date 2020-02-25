3aed355bea544b6741fd9bc5efea690b3e490235
oriaki1231 <aki.raliver.1231@icloud.com>
2020-02-25 16:23:15 +0900

    update

--- app/controllers/souls_controller.rb
+++ app/controllers/souls_controller.rb
@@ -1,14 +1,14 @@
 class SoulsController < ApplicationController
 
+  before_action :authenticate_user!
+  
   def new
     @soul = Soul.includes(:user)
     @souls = Soul.new
   end
 
   def create
-    
     Soul.create(soul_params)
-    # binding.pry
     redirect_to new_soul_path
   end
 
